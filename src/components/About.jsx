e0e1e321bd02b7548eb8c0cc031323f958a856a6
anubhav verma <anubhav.verma1506@gmail.com>
2023-06-17 13:40:05 +0530

    bugs fixes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -76,7 +76,7 @@ export default function About() {
           </div>
         </div>
 
-        <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 sm:hidden">
+        <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
           <img
             src={AboutImg}
             alt="cover-img"
